130c61bc9ef608ad656dd8ffe3071169ab672620
junhao <547455277@qq.com>
2019-04-18 19:02:26 +0800

    input >50000

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -394,12 +394,12 @@ export default {
         if (this.buttonVal === '充值') {
           this.submitOrderMatch('充值')
         } else {
-          if (parseInt(headerInfo.amount_income) < 10) {
+          if (parseInt(this.headerInfo.amount_income) < 10) {
             this.keyword = 10
             this.$toast('最小提现10积分')
             return
           }
-          if (parseInt(headerInfo.amount_income) > 50000) {
+          if (parseInt(this.headerInfo.amount_income) > 50000) {
             this.keyword = 50000
             this.$toast('最大提现50000积分')
             return
